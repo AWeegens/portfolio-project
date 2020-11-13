215ec9d74f0f02e9618949b81f525a6899fabc43
AWeegens <amchanc4600@gmail.com>
2020-11-13 11:17:14 -0500

    nav work

---
+++ js/basic.js
@@ -0,0 +1,13 @@
+//$(image).animate({left: '-500px'});
+
+/*const storkJS = document.getElementById('storkJS');
+
+storkJS.addEventListener('click', () => {
+    storkJS.animate({left: '-500px'});
+}); */
+/*
+function Move() {
+    var elem = document.getElementById("AnimationContainer);
+    var pos = 0;
+    var id
+}
