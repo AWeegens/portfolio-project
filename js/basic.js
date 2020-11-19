1d5e850c695965e2500447ee00612ea8ed540643
AWeegens <amchanc4600@gmail.com>
2020-11-18 19:50:22 -0500

    comments

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,9 @@
+// Create const for the question and make sure "yes" works regardless of case
 const pregnant = prompt ("Are you pregnant?");
 
 const pregnantAnswer = pregnant.toUpperCase();
+
+// If clause to calculate weeks left of pregnancy
 
 if(pregnantAnswer==="YES") {
     const weeks = prompt ("How many weeks along are you?");
